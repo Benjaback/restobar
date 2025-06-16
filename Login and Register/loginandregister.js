00e10e3eb38b33ec7f066f6fca12050046b4a8d1
Loa02172 <loanavillarroel12@gmail.com>
2025-06-16 01:11:52 -0300

    Plantilla de Login and Register

---
+++ Login and Register/loginandregister.js
@@ -0,0 +1,9 @@
+const contenedor = document.getElementById('contenedor');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+registerBtn.addEventListener('click', () => {
+    contenedor.classList.add("active");
+});
+loginBtn.addEventListener('click', () => {
+    contenedor.classList.remove("active");
+});
